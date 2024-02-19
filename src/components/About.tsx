1230322efbb0fcbf8ada156015e21c6e3ac28183
focusonyoursite <focusonyoursite@gmail.com>
2024-02-19 20:37:08 +0000

    qwqweqwe

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,21 +7,21 @@ export const About = () => {
       id="about"
       className="container py-24 sm:py-32"
     >
-      <div className="bg-muted/50 border rounded-lg py-12">
-        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+      <div className="py-12 border rounded-lg bg-muted/50">
+        <div className="flex flex-col-reverse gap-8 px-6 md:flex-row md:gap-12">
           
-          <div className="bg-green-0 flex flex-col justify-between">
+          <div className="flex flex-col justify-between bg-green-0">
             <div className="pb-6">
-              <h2 className="text-3xl md:text-4xl font-bold">
-                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+              <h2 className="text-3xl font-bold md:text-4xl">
+                <span className="text-transparent bg-gradient-to-b from-primary/60 to-primary bg-clip-text">
                   Is {" "}
                 </span>
                 Dennis Schouten   
-                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text ml-3">
+                 <span className="ml-3 text-transparent bg-gradient-to-b from-primary/60 to-primary bg-clip-text">
                     al dood?{" "}
                 </span>
               </h2>
-              <p className="text-xxl text-muted-foreground mt-4">
+              <p className="mt-4 text-xxl text-muted-foreground">
                 Nope, Dennis leeft nog! 
               </p>
             </div> 
